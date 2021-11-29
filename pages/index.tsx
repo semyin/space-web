e6296e613db1ec7d110c5dcbdc45127e20ada68a
semyin <semyin@kplocker.com>
2021-11-29 15:27:55 +0800

    首页取数据

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,45 +7,66 @@ import type {AppProps} from "next/app";
 import {GetServerSideProps} from "next";
 import {useCookies} from "react-cookie";
 import {cookiePrefix} from "@/config/constant";
-import {IInitData} from "@/types";
+import {IArticle, IInitData, IPage} from "@/types";
 import style from '@/styles/pages/index.module.scss'
 import {parseCookies} from "@/utils/parseCookies";
 import {getRequestAuthHeader} from "@/utils/auth";
+import request from "@/plugins/request";
+import {serverApi} from "@/api";
 
-export default function Page(props: AppProps) {
 
+type Props = {
+  articleList: IArticle[]
+}
+
+export default function Page({ articleList }: Props) {
+  console.log(articleList)
   return (
     <section className={style.home}>
       <div className={style.wrap}>
         <div className={style.left}>
           <div className={style.articleList}>
-            <div className={style.item}>
-              <div className={style.coverImg}>
-
-              </div>
-              <div className={style.content}>
-                <div className={style.title}>
-
-                </div>
-                <div className={style.brief}>
-
-                </div>
-                <div className={style.bottom}>
-                  <div className={style.bottomLeft}>
-                    <div className={style.time}>
-
+            {
+              articleList.map(item => {
+                return (
+                  <div key={item.id} className={style.item}>
+                    <div className={style.coverImg}>
+                      <img src={item.coverImg} alt=""/>
                     </div>
-                    <div className={style.tagList}>
-
+                    <div className={style.content}>
+                      <div className={style.title}>
+                        {item.title}
+                      </div>
+                      <div className={style.brief}>
+                        {item.brief}
+                      </div>
+                      <div className={style.bottom}>
+                        <div className={style.bottomLeft}>
+                          <div className={style.time}>
+                            {item.createdAt}
+                          </div>
+                          <div className={style.tagList}>
+                            {
+                              item.tagList.map(tag => {
+                                return (
+                                  <div key={tag.id}>{tag.tagName}</div>
+                                )
+                              })
+                            }
+                          </div>
+                        </div>
+                        <div className={style.bottomRight}>
+                          <span>{item.viewNum}</span>
+                          <span>{item.commentNum}</span>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                  <div className={style.bottomRight}>
-                    浏览，评论
-                  </div>
-                </div>
-              </div>
 
-            </div>
+                  </div>
+                )
+              })
+            }
+
           </div>
         </div>
         <div className={style.right}>
@@ -57,11 +78,27 @@ export default function Page(props: AppProps) {
   )
 }
 
+async function getArticleList(params: IPage) {
+  try {
+    const res = await request.get(serverApi.article, {
+      params
+    })
+    return res.data.data || []
+  } catch (e) {
+    console.log(e)
+  }
+}
+
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const header = getRequestAuthHeader(context.req)
-  console.log(header)
+  const params: IPage = {
+    currentPage: 1,
+    pageSize: 10
+  }
+  const articleList = await getArticleList(params)
   return {
-    props: {}
+    props: {
+      articleList
+    }
   }
 }
 
--- types/index.ts
+++ types/index.ts
@@ -5,6 +5,11 @@ export declare interface IResponse<T> {
   data: T
 }
 
+export declare interface IPage {
+  currentPage: number,
+  pageSize: number
+}
+
 export declare interface IMenu {
   id: number,
   typeName: string
@@ -26,4 +31,27 @@ export declare interface IUserInfo {
 export declare interface IInitData {
   menu: IMenu[],
   loginStatus: boolean
+}
+
+export declare interface ITagList {
+  id: string
+  tagId: number
+  tagName: string
+  targetId: string
+}
+
+export declare interface IArticle {
+  articleType: number
+  brief: string
+  coverImg: string
+  createdAt: number
+  detail: any
+  id: string
+  status: number
+  tagList: ITagList[]
+  title: string
+  updatedAt: number
+  userId: string
+  viewNum: number,
+  commentNum: number
 }
